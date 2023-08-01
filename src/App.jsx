ac63fadb5eacfc41e6dea3b98232f886757171ae
Atif Aiman <alserembani94@gmail.com>
2023-08-01 23:39:08 +0800

    add mui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Button variant="contained" color="error">
+        Text
+      </Button>
+    </>
+  );
+}
+
+export default App;
